ea7e30d1e7825ae32d5504bce9c48e1ce3f4f279
Kmcdonnell20 <kalebmcdonnell24@gmail.com>
2022-03-09 15:55:10 -0800

    Updated Window file

---
+++ code/MainWindow/config.h
@@ -0,0 +1,23 @@
+#pragma once
+// File with all of the constants
+
+
+#ifndef config
+#define config
+
+// Force Redraw timer
+#define TIMER 16.6          // 60 frame per second
+
+// Window settings
+const int WIN_WIDTH = 640;
+const int WIN_HEIGHT = 480;
+
+// Player Settings
+const int PLAYER_WIDTH = 100;
+const int PLAYER_HEIGHT = 5;
+
+// Ball settings
+const float BALL_RADIUS = 6.0f;
+const int CIRCLE_SEGMENTS = 50; // used in drawing ball and the life/heart symbol
+
+#endif
